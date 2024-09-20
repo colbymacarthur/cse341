e56f0e06ab8b92988858f3453905049579c675d0
Colby MacArthur <colby.mac89@gmail.com>
2024-09-20 13:20:36 -0600

    ..

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-
+ 
 app.get('/', (req, res) => {
-    res.send("Good bye");
+  res.send("Hello");
 });
 
 const port = 3000;
-   
-app.listen(process.env.PORT || port,);
-console.log('Web Server is listening at port ' + (process.env.PORT || port));
+
+app.listen(process.env.PORT || port, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || port));
+});
